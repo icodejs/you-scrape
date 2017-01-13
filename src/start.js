1c9ff062a143180ec6b98233dff61fece60ec81b
Tahir J <tahir.joseph@thetrainline.com>
2017-01-13 17:59:51 +0000

    WIP

--- src/start.js
+++ src/start.js
@@ -1,3 +1,28 @@
+/*
+more
+yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon no-icon-markup pause-resume-autoplay yt-uix-menu-trigger yt-uix-tooltip
+*/
+
+/*
+menu
+yt-uix-menu-content yt-ui-menu-content yt-uix-kbd-nav
+*/
+
+/*
+  transcript button
+  yt-ui-menu-item has-icon yt-uix-menu-close-on-select action-panel-trigger action-panel-trigger-transcript
+*/
+
+/*
+  transcript-scrollbox
+
+  #transcript-scrollbox
+
+  or
+
+  #watch-actions-transcript
+*/
+
 import phantom from 'phantom';
 
 (async function() {
@@ -18,25 +43,50 @@ import phantom from 'phantom';
     };
   });
 
-  page.evaluate(function() {
-    window.navigator = {
-      plugins: { 'Shockwave Flash': { description: 'Shockwave Flash 11.2 e202' } },
-      mimeTypes: { 'application/x-shockwave-flash': { enabledPlugin: true } }
-    };
+
+  await page.on('onResourceRequested', function(requestData) {
+    console.info('Requesting', requestData.url);
   });
 
-  // await page.on('onResourceRequested', function(requestData) {
-  //   console.info('Requesting', requestData.url);
+
+  // await page.on('onError', function(msg, trace) {
+  //   var msgStack = ['ERROR: ' + msg];
+
+  //   if (trace && trace.length) {
+  //     msgStack.push('TRACE:');
+  //     trace.forEach(function(t) {
+  //       msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+  //     });
+  //   }
+
+  //   console.error(msgStack.join('\n'));
   // });
 
   const status = await page.open(url);
   console.log(status);
 
 
-  const html = await page.evaluate(function() {
-    return document.querySelector('.add-to-widget').innerHTML;
+
+
+  await page.evaluate(function() {
+    return document.querySelector('.yt-uix-button-has-icon').click();
   });
-  console.log(html);
+
+  await page.evaluate(function() {
+    return document.querySelector('.action-panel-trigger-transcript').click();
+  });
+
+  const t = await new Promise((resolve) => {
+    setTimeout(async () => {
+      const x = await page.evaluate(function() {
+        return document.querySelector('#watch-actions-transcript').innerHTML;
+      });
+      resolve(x);
+    }, 20000);
+  });
+
+
+  console.log(t);
 
   await instance.exit();
 }());
