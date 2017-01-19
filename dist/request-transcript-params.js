b10fa2eddd920b9d49cb53bbde57e3f0f65b1416
Tahir J <tahir.joseph@thetrainline.com>
2017-01-19 12:54:53 +0000

    Make it easier to crock the code

--- dist/request-transcript-params.js
+++ dist/request-transcript-params.js
@@ -4,8 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 var _phantom = require('phantom');
@@ -26,153 +24,92 @@ var _lodash2 = _interopRequireDefault(_lodash);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; } /* eslint-disable no-console */
 
 exports.default = function () {
-  var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee3(videoId, fn) {
-    var _this = this;
-
-    var callback, _ret;
-
-    return regeneratorRuntime.wrap(function _callee3$(_context3) {
+  var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(videoId, fn) {
+    var instance, page, pageUrl, callback;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
-        switch (_context3.prev = _context3.next) {
+        switch (_context.prev = _context.next) {
           case 0:
-            callback = (0, _lodash2.default)(fn);
-            _context3.prev = 1;
-            return _context3.delegateYield(regeneratorRuntime.mark(function _callee2() {
-              var instance, page, pageUrl, status;
-              return regeneratorRuntime.wrap(function _callee2$(_context2) {
-                while (1) {
-                  switch (_context2.prev = _context2.next) {
-                    case 0:
-                      _context2.next = 2;
-                      return _phantom2.default.create();
+            _context.next = 2;
+            return _phantom2.default.create();
 
-                    case 2:
-                      instance = _context2.sent;
-                      _context2.next = 5;
-                      return instance.createPage();
+          case 2:
+            instance = _context.sent;
+            _context.next = 5;
+            return instance.createPage();
 
-                    case 5:
-                      page = _context2.sent;
-                      pageUrl = 'https://youtu.be/' + videoId;
+          case 5:
+            page = _context.sent;
+            pageUrl = 'https://youtu.be/' + videoId;
+            callback = (0, _lodash2.default)(function (error, response) {
+              fn(error, response);
+              instance.exit();
+            });
 
 
-                      page.on('onResourceRequested', function (requestData) {
-                        var transcriptUrl = requestData.url;
+            try {
+              page.on('onResourceRequested', function (requestData) {
+                var transcriptUrl = requestData.url;
 
-                        if (transcriptUrl.indexOf('timedtext') > -1) {
-                          var _transcriptUrl$split = transcriptUrl.split('?'),
-                              _transcriptUrl$split2 = _slicedToArray(_transcriptUrl$split, 2),
-                              domain = _transcriptUrl$split2[0],
-                              qstring = _transcriptUrl$split2[1];
 
-                          var params = _querystring2.default.parse(qstring);
-                          console.log('transcript resource found:', transcriptUrl);
-                          callback(null, {
-                            url: domain,
-                            params: params
-                          });
-                        }
-                      });
+                if (transcriptUrl.indexOf('timedtext') > -1) {
+                  var _transcriptUrl$split = transcriptUrl.split('?'),
+                      _transcriptUrl$split2 = _slicedToArray(_transcriptUrl$split, 2),
+                      domain = _transcriptUrl$split2[0],
+                      qstring = _transcriptUrl$split2[1];
 
-                      page.on('onError', function (msg, trace) {
-                        var msgStack = ['ERROR: ' + msg];
+                  var params = _querystring2.default.parse(qstring);
 
-                        if (trace && trace.length) {
-                          msgStack.push('TRACE:');
-                          trace.forEach(function (t) {
-                            msgStack.push(' -> ' + t.file + ': ' + t.line + ' ' + t.function);
-                          });
-                        }
+                  console.log('transcript resource found:', transcriptUrl);
 
-                        callback(msgStack.join('\n'));
-                        return instance.exit();
-                      });
-
-                      _context2.next = 11;
-                      return page.open(pageUrl);
-
-                    case 11:
-                      status = _context2.sent;
-
-                      console.log('status:', status, pageUrl);
-
-                      if (!(status !== 'success')) {
-                        _context2.next = 16;
-                        break;
-                      }
-
-                      callback(status);
-                      return _context2.abrupt('return', {
-                        v: instance.exit()
-                      });
-
-                    case 16:
-                      _context2.next = 18;
-                      return new Promise(function (resolve) {
-                        setTimeout(_asyncToGenerator(regeneratorRuntime.mark(function _callee() {
-                          return regeneratorRuntime.wrap(function _callee$(_context) {
-                            while (1) {
-                              switch (_context.prev = _context.next) {
-                                case 0:
-                                  return _context.abrupt('return', resolve());
-
-                                case 1:
-                                case 'end':
-                                  return _context.stop();
-                              }
-                            }
-                          }, _callee, _this);
-                        })), (0, _sek2.default)(10));
-                      });
-
-                    case 18:
-                      _context2.next = 20;
-                      return page.evaluate(function () {
-                        document.querySelector('.yt-uix-button-has-icon').click();
-                        document.querySelector('.action-panel-trigger-transcript').click();
-                      });
-
-                    case 20:
-
-                      instance.exit();
-
-                    case 21:
-                    case 'end':
-                      return _context2.stop();
-                  }
+                  callback(null, {
+                    url: domain,
+                    params: params
+                  });
                 }
-              }, _callee2, _this);
-            })(), 't0', 3);
+              });
 
-          case 3:
-            _ret = _context3.t0;
+              page.on('onError', function (msg, trace) {
+                var msgStack = ['ERROR: ' + msg];
 
-            if (!((typeof _ret === 'undefined' ? 'undefined' : _typeof(_ret)) === "object")) {
-              _context3.next = 6;
-              break;
+                if (trace && trace.length) {
+                  msgStack.push('TRACE:');
+                  trace.forEach(function (t) {
+                    msgStack.push(' -> ' + t.file + ': ' + t.line + ' ' + t.function);
+                  });
+                }
+
+                callback(msgStack.join('\n'));
+                return instance.exit();
+              });
+
+              page.open(pageUrl).then(function (status) {
+                console.log('status:', status, pageUrl);
+
+                if (status !== 'success') {
+                  return callback(status);
+                }
+
+                setTimeout(function () {
+                  page.evaluate(function () {
+                    document.querySelector('.yt-uix-button-has-icon').click();
+                    document.querySelector('.action-panel-trigger-transcript').click();
+                  });
+                }, (0, _sek2.default)(10));
+              });
+            } catch (e) {
+              callback(e);
             }
 
-            return _context3.abrupt('return', _ret.v);
-
-          case 6:
-            _context3.next = 11;
-            break;
-
-          case 8:
-            _context3.prev = 8;
-            _context3.t1 = _context3['catch'](1);
-
-            callback(_context3.t1);
-
-          case 11:
+          case 9:
           case 'end':
-            return _context3.stop();
+            return _context.stop();
         }
       }
-    }, _callee3, this, [[1, 8]]);
+    }, _callee, this);
   }));
 
   return function (_x, _x2) {
--- src/request-transcript-params.js
+++ src/request-transcript-params.js
@@ -1,22 +1,29 @@
+/* eslint-disable no-console */
+
 import phantom from 'phantom';
 import sek from 'sek';
 import querystring from 'querystring';
 import once from 'lodash.once';
 
 export default async function(videoId, fn) {
-  const callback = once(fn);
+  const instance = await phantom.create();
+  const page = await instance.createPage();
+  const pageUrl = `https://youtu.be/${videoId}`;
+  const callback = once((error, response) => {
+    fn(error, response);
+    instance.exit();
+  });
 
   try {
-    const instance = await phantom.create();
-    const page = await instance.createPage();
-    const pageUrl = `https://youtu.be/${videoId}`;
-
     page.on('onResourceRequested', function(requestData) {
       const { url: transcriptUrl } = requestData;
+
       if (transcriptUrl.indexOf('timedtext') > -1) {
         const [ domain, qstring ] = transcriptUrl.split('?');
         const params = querystring.parse(qstring);
+
         console.log('transcript resource found:', transcriptUrl);
+
         callback(null, {
           url: domain,
           params
@@ -38,24 +45,20 @@ export default async function(videoId, fn) {
       return instance.exit();
     });
 
-    const status = await page.open(pageUrl);
-    console.log('status:', status, pageUrl);
+    page.open(pageUrl).then((status) => {
+      console.log('status:', status, pageUrl);
 
-    if (status !== 'success') {
-      callback(status);
-      return instance.exit();
-    }
+      if (status !== 'success') {
+        return callback(status);
+      }
 
-    await new Promise((resolve) => {
-      setTimeout(async () => resolve(), sek(10));
+      setTimeout(() => {
+        page.evaluate(function() {
+          document.querySelector('.yt-uix-button-has-icon').click();
+          document.querySelector('.action-panel-trigger-transcript').click();
+        });
+      }, sek(10));
     });
-
-    await page.evaluate(function() {
-      document.querySelector('.yt-uix-button-has-icon').click();
-      document.querySelector('.action-panel-trigger-transcript').click();
-    });
-
-    instance.exit();
   } catch (e) {
     callback(e);
   }
